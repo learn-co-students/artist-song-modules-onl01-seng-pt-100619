8c27cf4ce0655aea32cecbabefebe224a1e73326
Kevin Hayden <kevin.fhayden@gmail.com>
2019-11-04 22:45:01 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,16 +1,17 @@
 require 'pry'
 
 class Artist
-  extend Memorable
+  extend Memorable::ClassMethods
   extend Findable
   include Paramable
+  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
   def initialize
-    @@artists << self
+    super
     @songs = []
   end
 
